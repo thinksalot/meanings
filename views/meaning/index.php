023793aa41d3d24aa677085fc324617fac3a836a
thinksalot <thapa.aalok@gmail.com>
2016-01-03 17:36:30 +0545

    styling the meaning list

--- views/meaning/index.php
+++ views/meaning/index.php
@@ -1,26 +1,22 @@
-<table style='' class='table table-striped table-bordered table-condensed table-responsive'>
-    <thead>
-        <tr>
-            <th style='text-align: left; width: 50px'>#Id</th>
-            <th style='text-align: left;'>Word</th>
-            <th style='text-align: left; width: 200px'>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach( $words as $word ): ?>
-        <tr>
-            <td><?= $word->id ?></td>
-            <td>
-                <span data-toggle="tooltip" data-html="true" title="<?= htmlspecialchars( nl2br( $word->meaning ), ENT_QUOTES, 'utf-8' ) ?>">
-                    <em><?= $word->word ?></em>
-                </span>
-            </td>
-            <td>
-                <a href="/view/<?= $word->id ?>">View</a>
-                <a href="/edit/<?= $word->id ?>">Edit</a>
-                <a href="/delete/<?= $word->id ?>">Delete</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="items">
+    <?php foreach( $words as $word ): ?>
+    <div class="item_details">
+        <div class="term">
+            <strong> <?= $word->word ?> </strong>
+        </div>
+        <div class="definition">
+            <?= nl2br( $word->meaning ) ?>
+        </div>
+        <div class="actions">
+            <div class="buttons">
+                <a class='btn btn-default btn-xs' href="/edit/<?= $word->id ?>">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                </a>
+                <a class='btn btn-default btn-xs' href="/delete/<?= $word->id ?>">
+                    <span class="glyphicon glyphicon-trash"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</div>
