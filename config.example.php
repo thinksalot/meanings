c7004d68335b7f1cdfa65cd7b2db614dc1695da6
thinksalot <thapa.aalok@gmail.com>
2016-01-07 09:00:30 +0545

    updating config example

--- config.example.php
+++ config.example.php
@@ -2,6 +2,13 @@
 
 define( 'DB_HOST', 'localhost' );
 define( 'DB_NAME', 'meanings' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_USER', '' );
+define( 'DB_PASSWORD', '' );
+
+define( 'SITE_URL', 'http://meanings/' );
 define( 'VIEW_DIR', 'views/' );
+
+define( 'QUIZLET_CLIENT_ID', '' );
+define( 'QUIZLET_CLIENT_SECRET', '' );
+
+define( 'QUIZLET_SET_ID', '' );
